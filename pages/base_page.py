43126d7bc02eee13dfbb4631c7afaf37d4f908e4
Luckymaice <kayumov.m78@mail.ru>
2025-07-01 20:59:02 +0300

    added check for items

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,9 +2,17 @@
 
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, that):
+        try:
+            self.browser.find_element(how, that)
+        except:
+            return False
+        return True
